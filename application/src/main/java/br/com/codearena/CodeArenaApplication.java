400b6684db0d0be25711563576041e33d26fa848
gabrielgouv <joao.gabriel.gouveia2@gmail.com>
2019-11-03 23:56:37 -0300

    Initial commit / Base Project

---
+++ application/src/main/java/br/com/codearena/CodeArenaApplication.java
@@ -0,0 +1,13 @@
+package br.com.codearena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeArenaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeArenaApplication.class, args);
+    }
+
+}
---
+++ core/src/main/java/br/com/codearena/Teste.java
@@ -0,0 +1,5 @@
+package br.com.codearena;
+
+public class Teste {
+
+}
