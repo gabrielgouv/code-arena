b1757739161a0e1cf0bb375c0b52d4bf5c8e14db
jgga <joao.gouveia@pitang.com>
2019-11-07 15:02:30 -0300

    Adicionada opção de configurar o properties do sistema via variáveis de ambiente do sistema

--- code-arena-server/application/src/main/java/br/com/codearena/CodeArenaApplication.java
+++ code-arena-server/application/src/main/java/br/com/codearena/CodeArenaApplication.java
@@ -1,15 +1,27 @@
 package br.com.codearena;
 
-//import br.com.codearena.core.configuration.Constants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication()
 public class CodeArenaApplication {
 
+    private static final String CODE_ARENA_PROPERTIES_HOME = "CODE_ARENA_PROPERTIES_HOME";
+
     public static void main(String[] args) {
-//        System.setProperty("spring.config.location", Constants.DEFAULT_PROPERTIES_FILE_PATH);
+
+        checkForExternalProperties();
+
         SpringApplication.run(CodeArenaApplication.class, args);
+
+    }
+
+    private static void checkForExternalProperties() {
+        String springConfigLocationHome = System.getenv(CODE_ARENA_PROPERTIES_HOME);
+
+        if (springConfigLocationHome != null && !"".equals(springConfigLocationHome.trim())) {
+            System.setProperty("spring.config.location", CODE_ARENA_PROPERTIES_HOME);
+        }
     }
 
 }
