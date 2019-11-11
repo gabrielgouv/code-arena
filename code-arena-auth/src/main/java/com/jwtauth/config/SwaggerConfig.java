e4cdb1dd4bbc137c3dbbac5066ceb13aabd46d32
João Gabriel Gouveia <joao.gabriel.gouveia2@gmail.com>
2019-11-10 23:56:48 -0300

    Corrigo erro no arquivo

--- code-arena-auth/src/main/java/com/jwtauth/config/SwaggerConfig.java
+++ code-arena-auth/src/main/java/com/jwtauth/config/SwaggerConfig.java
@@ -14,30 +14,31 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig extends WebMvcConfigurationSupport {
-@Bean
-public Docket productApi() {
-return new Docket(DocumentationType.SWAGGER_2)
-.select()
-.apis(RequestHandlerSelectors.basePackage(“com.crm”))
-.build()
-.apiInfo(metaData());
-}
 
-private ApiInfo metaData() {
-return new ApiInfoBuilder()
-.title(“Spring Boot REST API”)
-.description(“\”Spring Boot REST API\””)
-.version(“1.0.0”)
-.license(“Apache License Version 2.0”)
-.licenseUrl(“https://www.apache.org/licenses/LICENSE-2.0\"")
-.build();
-}
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.crm"))
+            .build()
+            .apiInfo(metaData());
+    }
 
-@Override
-protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-registry.addResourceHandler(“swagger-ui.html”)
-.addResourceLocations(“classpath:/META-INF/resources/”);
-registry.addResourceHandler(“/webjars/**”)
-.addResourceLocations(“classpath:/META-INF/resources/webjars/”);
-}
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+            .title("Spring Boot REST API")
+            .description("Spring Boot REST API")
+            .version("1.0.0")
+            .license("Apache License Version 2.0")
+            .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+            .build();
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+            .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }
