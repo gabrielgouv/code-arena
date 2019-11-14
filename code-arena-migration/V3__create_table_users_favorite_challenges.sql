b674412d16aa71c13a6b6bd57796b8994c35ed21
jgga <joao.gouveia@pitang.com>
2019-11-14 14:07:21 -0300

    Ajuste na criação da tabela

--- code-arena-migration/V3__create_table_users_favorite_challenges.sql
+++ code-arena-migration/V3__create_table_users_favorite_challenges.sql
@@ -1,13 +1,15 @@
-CREATE TABLE users_favorite_challenges
+CREATE TABLE user_favorite_challenges
 (
-    users_who_favorited_id bigint NOT NULL,
-    favorite_challenges_id bigint NOT NULL,
-    CONSTRAINT users_who_favorited_fkey FOREIGN KEY (users_who_favorited_id)
-        REFERENCES public.users (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT favorite_challenges_fkey FOREIGN KEY (favorite_challenges_id)
-        REFERENCES public.challenges (id) MATCH SIMPLE
+    user_id bigint NOT NULL,
+    challenge_id bigint NOT NULL,
+    CONSTRAINT user_favorite_challenges_challenge_fkey FOREIGN KEY (challenge_id)
+        REFERENCES challenges (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
-);
+        NOT VALID,
+    CONSTRAINT user_favorite_challenges_user_fkey FOREIGN KEY (user_id)
+        REFERENCES users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
