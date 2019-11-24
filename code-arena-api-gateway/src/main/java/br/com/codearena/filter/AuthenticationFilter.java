3fe1ce7455aa1dc5480752f29f45e421600baa26
João Gabriel Gouveia <joao.gabriel.gouveia2@gmail.com>
2019-11-24 17:52:48 -0300

    Ajustes

--- code-arena-api-gateway/src/main/java/br/com/codearena/filter/AuthenticationFilter.java
+++ code-arena-api-gateway/src/main/java/br/com/codearena/filter/AuthenticationFilter.java
@@ -2,6 +2,7 @@ package br.com.codearena.filter;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -19,14 +20,30 @@ public class AuthenticationFilter extends ZuulFilter {
 
     @Override
     public boolean shouldFilter() {
-        return true;
+        RequestContext currentContext = RequestContext.getCurrentContext();
+        String uri = currentContext.getRequest().getRequestURI();
+
+        // Filtra tudo exceto a API de autenticação
+        return !uri.startsWith("/auth");
     }
 
     @Override
     public Object run() {
         RequestContext currentContext = RequestContext.getCurrentContext();
-        // Adiciona o código do usuário autenticado no header, para ser pego no backstream (code-arena-api)
-        currentContext.addZuulRequestHeader("logged-user-id", "1");
+
+        // TODO: Fazer a requisição para o servidor de autorização:
+        boolean autenticado = true;
+
+        // Impede da requisição continuar caso não esteja autorizado
+        if (!autenticado) {
+            currentContext.setSendZuulResponse(false);
+            currentContext.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+        } else {
+            // TODO: Mudar para passar o objeto usuário inteiro
+            // Adiciona o código do usuário autenticado no header, para ser pego no backstream (code-arena-api)
+            currentContext.addZuulRequestHeader("logged-user-id", "1");
+        }
+
         return null;
     }
 
