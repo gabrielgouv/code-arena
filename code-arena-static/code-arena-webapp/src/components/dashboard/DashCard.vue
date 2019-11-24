e508613fac7c32b7eb847447f14b4dbeee446abc
João Gabriel Gouveia <joao.gabriel.gouveia2@gmail.com>
2019-11-24 12:07:11 -0300

    Criado componente DashCard

---
+++ code-arena-static/code-arena-webapp/src/components/dashboard/DashCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="dash-card">
+    <vs-card actionable class="cardx">
+        <div slot="header">
+            <h3>{{ title }}</h3>
+            <h6 class="card-subtitle">{{ category ? category.toUpperCase() : category }}</h6>
+        </div>
+        <br/>
+        <div>
+            <vs-progress :height="12" :percent="percentDone" :color="challengesDone > 0 ? 'success' : 'primary'">success</vs-progress>
+            <template>
+                <span v-if="challengesDone > 0">
+                    <strong>{{ percentDone }}%</strong> ({{ challengesDone }}/{{ totalChallenges }} challenges solved)
+                </span>
+                <span v-else>
+                    Not started
+                </span>
+            </template>
+            
+        </div>
+        <br/>
+        <vs-button v-if="challengesDone > 0" class="card-button" color="success" type="gradient">Continue</vs-button>
+        <vs-button v-else class="card-button" color="primary" type="gradient">Start Coding!</vs-button>
+    </vs-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DashCard',
+  props: [
+      'title',
+      'category',
+      'challengesDone',
+      'totalChallenges'
+  ],
+  computed: {
+      percentDone() {
+          return (this.challengesDone / this.totalChallenges) * 100;
+      }
+  }
+}
+</script>
+
+<style scoped>
+.con-vs-card {
+    padding: 5px 5px 0 5px;
+}
+.card-button {
+    width: 100%;
+}
+.card-subtitle {
+    color: #868686;
+    margin-top: 5px;
+}
+</style>
--- code-arena-static/code-arena-webapp/src/views/Dashboard.vue
+++ code-arena-static/code-arena-webapp/src/views/Dashboard.vue
@@ -3,50 +3,27 @@
     <PageTitle title="Dashboard" subtitle="Your dashboard"/>
     <br/>
     <br/>
-    <br/>
     <b-row>
         <b-col md="12">
             <vs-row vs-justify="center">
                 <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="5">
-                    <vs-card actionable class="cardx">
-                        <div slot="header">
-                            <h3>Java</h3>
-                            <h6>LANGUAGE PROFICIENCY</h6>
-                        </div>
-                        <br/>
-                        <div>
-                            <vs-progress :height="12" :percent="9" color="success">success</vs-progress>
-                            <span><strong>9%</strong> (6/69 challenges solved)</span>
-                        </div>
-                        <br/>
-                            <div slot="footer">
-                            <vs-row vs-justify="flex-end">
-                                <vs-button class="card-button" color="success" type="gradient" >Continue</vs-button>
-                            </vs-row>
-                        </div>
-                    </vs-card>
+                    <DashCard 
+                        title="Java" 
+                        category="Language Proficiency"
+                        challenges-done="5"
+                        total-challenges="10"
+                        />
                 </vs-col>
 
                 <vs-col vs-w="1"></vs-col>
 
                 <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="5">
-                    <vs-card actionable class="cardx">
-                        <div slot="header">
-                            <h3>Python</h3>
-                            <h6>LANGUAGE PROFICIENCY</h6>
-                        </div>
-                        <br/>
-                        <div>
-                            <vs-progress :height="12" :percent="0" color="primary">success</vs-progress>
-                            <span>Not started</span>
-                        </div>
-                        <br/>
-                            <div slot="footer">
-                            <vs-row vs-justify="flex-end">
-                                <vs-button class="card-button" color="primary" type="gradient" >Start Coding!</vs-button>
-                            </vs-row>
-                        </div>
-                    </vs-card>
+                    <DashCard 
+                        title="Python" 
+                        category="Language Proficiency"
+                        challenges-done="0"
+                        total-challenges="16"
+                        />
                 </vs-col>
                 
             </vs-row>
@@ -58,18 +35,14 @@
 
 <script>
 import PageTitle from '@/components/template/PageTitle'
+import DashCard from '@/components/dashboard/DashCard'
 
 export default {
   name: 'Dashboard',
-  components: { PageTitle }
+  components: { PageTitle, DashCard }
 }
 </script>
 
 <style scoped>
-.con-vs-card {
-    padding: 10px;
-}
-.card-button {
-    width: 100%;
-}
+
 </style>
